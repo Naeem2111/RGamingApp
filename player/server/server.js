2dfb231d5b89d087c328b383bf0a2ae2906800fe
Naeem <naeem2111@gmail.com>
2024-06-22 03:55:32 +0200

    server

---
+++ player/server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const sequelize = require('./db');
+const playerRoutes = require('./routes/playerRoutes');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/api', playerRoutes);
+
+// Sync database and start server
+sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+});
